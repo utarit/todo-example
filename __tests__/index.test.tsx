b09b4bf6c526d44e6f19c26e97667545c6988288
Mert Akca <mert.akca@plentific.com>
2023-05-12 02:48:12 +0300

    Install jest and write unit todos

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,12 @@
+// import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+// import Home from "@/app/page";
+
+it.todo("the user can add an item to the list");
+it.todo("the user can edit an item in the list");
+it.todo("the user can mark an item as done in the list");
+it.todo("automatically save the time when a new item is created");
+it.todo('shows a "last updated" time when editing an item in the list');
+it.todo("the user can check off an item, which archives it");
+it.todo("the user can view previously archived items");
