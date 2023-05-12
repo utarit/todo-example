99ad71bcfdf0b5c658669427c5ec2a4debb29526
Mert Akca <mert.akca@plentific.com>
2023-05-12 19:20:25 +0300

    Write unit tests for functional requirements

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,12 +1,129 @@
-// import { render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
-// import Home from "@/app/page";
+import Home from "@/app/page";
+import { DEFAULT_DATE_FORMAT, DEFAULT_LOCALE } from "@/utils/dateUtils";
 
-it.todo("the user can add an item to the list");
-it.todo("the user can edit an item in the list");
-it.todo("the user can mark an item as done in the list");
-it.todo("automatically save the time when a new item is created");
-it.todo('shows a "last updated" time when editing an item in the list');
-it.todo("the user can check off an item, which archives it");
-it.todo("the user can view previously archived items");
+it("the user can add an item to the list", async () => {
+  render(<Home />);
+  const user = userEvent.setup();
+
+  // Write a todo in the input which is labeled "Add to-do"
+  const input = screen.getByRole("textbox", { name: "Add to-do" });
+  await user.type(input, "Buy milk");
+
+  const addButton = screen.getByRole("button", { name: "Add" });
+  await user.click(addButton);
+  const currentDate = new Date();
+
+  // The todo should be in the list
+  expect(
+    screen.getByRole("listitem", { name: "Buy milk" })
+  ).toBeInTheDocument();
+
+  // Expect creation date
+  expect(screen.getByText("Created")).toBeInTheDocument();
+  expect(
+    screen.getByText(
+      currentDate.toLocaleDateString(DEFAULT_LOCALE, DEFAULT_DATE_FORMAT),
+      { exact: false }
+    )
+  ).toBeInTheDocument();
+});
+
+it("the user can edit an item in the list", async () => {
+  render(<Home />);
+  const user = userEvent.setup();
+
+  // Write a todo in the input
+  const input = screen.getByRole("textbox", { name: "Add to-do" });
+  await user.type(input, "Buy milk");
+
+  const addButton = screen.getByRole("button", { name: "Add" });
+  await user.click(addButton);
+  const createdDate = new Date();
+
+  // The todo should be in the list with edit button
+  const editButton = screen.getByRole("button", { name: "Edit" });
+  expect(editButton).toBeInTheDocument();
+
+  await user.click(editButton);
+
+  // Expect a text input with the value of to-do to be edited
+  const editInput = screen.getByRole("textbox", { name: "Edit to-do" });
+  expect(editInput).toHaveValue("Buy milk");
+
+  // Edit the input
+  await user.clear(editInput);
+  await user.type(editInput, "Buy eggs");
+
+  // Save changes
+  const saveButton = screen.getByRole("button", { name: "Save" });
+  await user.click(saveButton);
+  const editedDate = new Date();
+
+  // the todo should be changed with the new value
+  expect(
+    screen.queryByRole("listitem", { name: "Buy milk" })
+  ).not.toBeInTheDocument();
+  expect(
+    screen.getByRole("listitem", { name: "Buy eggs" })
+  ).toBeInTheDocument();
+
+  // Expect creation date
+  expect(screen.getByText("Created")).toBeInTheDocument();
+  expect(
+    screen.getByText(
+      createdDate.toLocaleDateString(DEFAULT_LOCALE, DEFAULT_DATE_FORMAT),
+      { exact: false }
+    )
+  ).toBeInTheDocument();
+
+  // Expect edited date
+  expect(screen.getByText("Last Updated")).toBeInTheDocument();
+  expect(
+    screen.getByText(
+      editedDate.toLocaleDateString(DEFAULT_LOCALE, DEFAULT_DATE_FORMAT),
+      { exact: false }
+    )
+  ).toBeInTheDocument();
+});
+
+it("the user can mark an item as done in the list and see it archived", async () => {
+  render(<Home />);
+  const user = userEvent.setup();
+
+  // Write a todo in the input which is labeled "Add to-do"
+  const input = screen.getByRole("textbox", { name: "Add to-do" });
+  await user.type(input, "Buy milk");
+
+  const addButton = screen.getByRole("button", { name: "Add" });
+  await user.click(addButton);
+
+  const todoList = screen.getByRole("list", { name: /to-do list/i });
+  const archivedList = screen.getByRole("list", { name: /archived list/i });
+
+  // The todo should be in the todo list
+  expect(
+    screen.getByRole("listitem", { name: "Buy milk" })
+  ).toBeInTheDocument();
+
+  expect(todoList.hasChildNodes).toBe(true);
+  expect(archivedList.hasChildNodes).toBe(false);
+
+  // Expect a checkbox to mark the item as done
+  const checkbox = screen.getByRole("checkbox", { name: "Mark as done" });
+  expect(checkbox).not.toBeChecked();
+
+  // Click checkbox to mark as done
+  await user.click(checkbox);
+
+  // The todo should be marked as done and gone from the list
+  expect(todoList.hasChildNodes).toBe(false);
+  expect(archivedList.hasChildNodes).toBe(true);
+
+  // Buy milk should appear in archived list and have line-through
+  const archivedItem = screen.queryByRole("listitem", { name: "Buy milk" });
+  expect(archivedItem).toHaveClass("line-through");
+});
---
+++ utils/dateUtils.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_LOCALE = "en-GB";
+
+export const DEFAULT_DATE_FORMAT = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: false,
+} satisfies Intl.DateTimeFormatOptions;
